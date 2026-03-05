9287ecb7011f2e6c4e0064e9ed266cbbefdb70c1
max-shakula <maxshakulakr@gmail.com>
2026-03-05 23:45:58 +0200

    done

---
+++ src/task-4.ts
@@ -0,0 +1,16 @@
+interface printUserInfo {
+  name: string;
+  age: number;
+  email?: string;
+}
+
+function printUserInfo(name: string, age: number, email?: string): void {
+  console.log("Name:", name);
+  console.log("Age:", age);
+  if (email) {
+    console.log("Email:", email);
+  }
+}
+
+printUserInfo("Alice", 30);
+printUserInfo("Bob", 25, "bob@mail.com");
